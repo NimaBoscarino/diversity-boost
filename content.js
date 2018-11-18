ba470e888df53858f492ba2c2abe0548353a0b5c
nboscari <nima_boscarino@sfu.ca>
2018-11-17 16:35:24 -0800

    button clears tweets

---
+++ content.js
@@ -0,0 +1,12 @@
+$(() => {
+  // create button
+  let filterButton = $('<button id="filter-button" class="js-global-new-tweet js-tooltip EdgeButton EdgeButton--primary js-dynamic-tooltip">Diversity Boost!</button>')
+
+  filterButton.click(() => {
+    let tweetsContainer = $('.stream-container')
+    tweetsContainer.empty()
+  })
+
+  // add button to nav
+  $('.right-actions').append(filterButton)
+})
