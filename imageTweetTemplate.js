a27cb5619b6d0af675337ccc3d2c62df8dd4b605
NimaBoscarino <nima.boscarino@gmail.com>
2019-01-30 16:58:49 -0600

    link up user + tweet id, avatars and content with imageTweetTemplate

--- imageTweetTemplate.js
+++ imageTweetTemplate.js
@@ -1,5 +1,5 @@
 const imageTweetTemplate = tweet => `<li class="js-stream-item stream-item stream-item
-" data-item-id="1090341261830045696" id="stream-item-tweet-1090341261830045696" data-item-type="tweet" data-suggestion-json="{&quot;suggestion_details&quot;:{},&quot;tweet_ids&quot;:&quot;1090341261830045696&quot;,&quot;scribe_component&quot;:&quot;tweet&quot;}">
+" data-item-id="${tweet.id_str}" id="stream-item-tweet-${tweet.id_str}" data-item-type="tweet" data-suggestion-json="{&quot;suggestion_details&quot;:{},&quot;tweet_ids&quot;:&quot;${tweet.id_str}&quot;,&quot;scribe_component&quot;:&quot;tweet&quot;}">
     
 
 
@@ -8,7 +8,7 @@ const imageTweetTemplate = tweet => `<li class="js-stream-item stream-item strea
       
       
        has-cards  has-content
-" data-tweet-id="1090341261830045696" data-item-id="1090341261830045696" data-permalink-path="/tenderlove/status/1090341261830045696" data-conversation-id="1090341261830045696" data-tweet-nonce="1090341261830045696-d5fe0be9-e710-419a-a15f-acedc7bdec63" data-tweet-stat-initialized="true" data-screen-name="tenderlove" data-name="Aaron Patterson" data-user-id="14761655" data-you-follow="true" data-follows-you="false" data-you-block="false" data-reply-to-users-json="[{&quot;id_str&quot;:&quot;14761655&quot;,&quot;screen_name&quot;:&quot;tenderlove&quot;,&quot;name&quot;:&quot;Aaron Patterson&quot;,&quot;emojified_name&quot;:{&quot;text&quot;:&quot;Aaron Patterson&quot;,&quot;emojified_text_as_html&quot;:&quot;Aaron Patterson&quot;}}]" data-disclosure-type="" data-has-cards="true">
+" data-tweet-id="${tweet.id_str}" data-item-id="${tweet.id_str}" data-permalink-path="/${tweet.user.screen_name}/status/${tweet.id_str}" data-conversation-id="${tweet.id_str}" data-tweet-nonce="${tweet.id_str}-d5fe0be9-e710-419a-a15f-acedc7bdec63" data-tweet-stat-initialized="true" data-screen-name="${tweet.user.screen_name}" data-name="${tweet.user.name}" data-user-id="${tweet.user.id_str}" data-you-follow="true" data-follows-you="false" data-you-block="false" data-reply-to-users-json="[{&quot;id_str&quot;:&quot;${tweet.user.id_str}&quot;,&quot;screen_name&quot;:&quot;${tweet.user.screen_name}&quot;,&quot;name&quot;:&quot;${tweet.user.name}&quot;,&quot;emojified_name&quot;:{&quot;text&quot;:&quot;${tweet.user.name}&quot;,&quot;emojified_text_as_html&quot;:&quot;${tweet.user.name}&quot;}}]" data-disclosure-type="" data-has-cards="true">
 
     <div class="context">
       
@@ -22,14 +22,14 @@ const imageTweetTemplate = tweet => `<li class="js-stream-item stream-item strea
 
       
       <div class="stream-item-header">
-          <a class="account-group js-account-group js-action-profile js-user-profile-link js-nav" href="/tenderlove" data-user-id="14761655">
-      <img class="avatar js-action-profile-avatar" src="https://pbs.twimg.com/profile_images/378800000325798111/ca48276f8ebbbbac9c6ce83aac3c8548_bigger.jpeg" alt="">
+          <a class="account-group js-account-group js-action-profile js-user-profile-link js-nav" href="/${tweet.user.screen_name}" data-user-id="${tweet.user.id_str}">
+      <img class="avatar js-action-profile-avatar" src="${tweet.user.profile_image_url}" alt="">
     <span class="FullNameGroup">
-      <strong class="fullname show-popup-with-id u-textTruncate " data-aria-label-part="">Aaron Patterson</strong><span>‏</span><span class="UserBadges"><span class="Icon Icon--verified"><span class="u-hiddenVisually">Verified account</span></span></span><span class="UserNameBreak">&nbsp;</span></span><span class="username u-dir u-textTruncate" dir="ltr" data-aria-label-part="">@<b>tenderlove</b></span></a>
+      <strong class="fullname show-popup-with-id u-textTruncate " data-aria-label-part="">${tweet.user.name}</strong><span>‏</span><span class="UserBadges"><span class="Icon Icon--verified"><span class="u-hiddenVisually">Verified account</span></span></span><span class="UserNameBreak">&nbsp;</span></span><span class="username u-dir u-textTruncate" dir="ltr" data-aria-label-part="">@<b>${tweet.user.screen_name}</b></span></a>
 
         
         <small class="time">
-  <a href="/tenderlove/status/1090341261830045696" class="tweet-timestamp js-permalink js-nav js-tooltip" title="12:09 PM - 29 Jan 2019" data-conversation-id="1090341261830045696"><span class="_timestamp js-short-timestamp js-relative-timestamp" data-time="1548792589" data-time-ms="1548792589000" data-long-form="true" aria-hidden="true">2h</span><span class="u-hiddenVisually" data-aria-label-part="last">2 hours ago</span></a>
+  <a href="/${tweet.user.screen_name}/status/${tweet.id_str}" class="tweet-timestamp js-permalink js-nav js-tooltip" title="12:09 PM - 29 Jan 2019" data-conversation-id="${tweet.id_str}"><span class="_timestamp js-short-timestamp js-relative-timestamp" data-time="1548792589" data-time-ms="1548792589000" data-long-form="true" aria-hidden="true">2h</span><span class="u-hiddenVisually" data-aria-label-part="last">2 hours ago</span></a>
 </small>
 
           <div class="ProfileTweet-action ProfileTweet-action--more js-more-ProfileTweet-actions">
@@ -53,14 +53,14 @@ const imageTweetTemplate = tweet => `<li class="js-stream-item stream-item strea
       <li class="embed-link js-actionEmbedTweet" data-nav="embed_tweet">
         <button type="button" class="dropdown-link">Embed Tweet</button>
       </li>
-          <li class="mute-user-item"><button type="button" class="dropdown-link">Mute <span class="username u-dir u-textTruncate" dir="ltr">@<b>tenderlove</b></span></button></li>
-    <li class="unmute-user-item"><button type="button" class="dropdown-link">Unmute <span class="username u-dir u-textTruncate" dir="ltr">@<b>tenderlove</b></span></button></li>
+          <li class="mute-user-item"><button type="button" class="dropdown-link">Mute <span class="username u-dir u-textTruncate" dir="ltr">@<b>${tweet.user.screen_name}</b></span></button></li>
+    <li class="unmute-user-item"><button type="button" class="dropdown-link">Unmute <span class="username u-dir u-textTruncate" dir="ltr">@<b>${tweet.user.screen_name}</b></span></button></li>
 
         <li class="block-link js-actionBlock" data-nav="block">
-          <button type="button" class="dropdown-link">Block <span class="username u-dir u-textTruncate" dir="ltr">@<b>tenderlove</b></span></button>
+          <button type="button" class="dropdown-link">Block <span class="username u-dir u-textTruncate" dir="ltr">@<b>${tweet.user.screen_name}</b></span></button>
         </li>
         <li class="unblock-link js-actionUnblock" data-nav="unblock">
-          <button type="button" class="dropdown-link">Unblock <span class="username u-dir u-textTruncate" dir="ltr">@<b>tenderlove</b></span></button>
+          <button type="button" class="dropdown-link">Unblock <span class="username u-dir u-textTruncate" dir="ltr">@<b>${tweet.user.screen_name}</b></span></button>
         </li>
       <li class="report-link js-actionReport" data-nav="report">
         <button type="button" class="dropdown-link">
@@ -92,7 +92,7 @@ const imageTweetTemplate = tweet => `<li class="js-stream-item stream-item strea
 
       
         <div class="js-tweet-text-container">
-  <p class="TweetTextSize TweetTextSize--normal js-tweet-text tweet-text" lang="en" data-aria-label-part="0">My cat likes to do extreme face licking<a href="https://t.co/iKqAqhhEQm" class="twitter-timeline-link u-hidden" data-pre-embedded="true" dir="ltr">pic.twitter.com/iKqAqhhEQm</a></p>
+  <p class="TweetTextSize TweetTextSize--normal js-tweet-text tweet-text" lang="en" data-aria-label-part="0">${tweet.text}<a href="${tweet.entities.media[0].url}" class="twitter-timeline-link u-hidden" data-pre-embedded="true" dir="ltr">pic.twitter.com/iKqAqhhEQm</a></p>
 </div>
 
 
@@ -101,16 +101,14 @@ const imageTweetTemplate = tweet => `<li class="js-stream-item stream-item strea
       
             <div class="AdaptiveMediaOuterContainer">
     <div class="AdaptiveMedia
-        
-        is-square
-        
+                
         
         
         ">
       <div class="AdaptiveMedia-container">
           <div class="AdaptiveMedia-singlePhoto" style="padding-top: calc(1.3333333333333333 * 100% - 0.5px);">
-    <div class="AdaptiveMedia-photoContainer js-adaptive-photo " data-image-url="https://pbs.twimg.com/media/DyGrmDbUcAAuqJq.jpg" data-element-context="platform_photo_card" style="background-color:rgba(64,50,37,1.0);" data-dominant-color="[64,50,37]">
-  <img data-aria-label-part="" src="https://pbs.twimg.com/media/DyGrmDbUcAAuqJq.jpg" alt="" style="width: 100%; top: -84px;">
+    <div class="AdaptiveMedia-photoContainer js-adaptive-photo " data-image-url="${tweet.entities.media[0].media_url}" data-element-context="platform_photo_card" style="background-color:rgba(64,50,37,1.0);" data-dominant-color="[64,50,37]">
+  <img data-aria-label-part="" src="${tweet.entities.media[0].media_url}" alt="" style="width: 100%;">
 </div>
 
 
@@ -133,24 +131,24 @@ const imageTweetTemplate = tweet => `<li class="js-stream-item stream-item strea
     
     <span class="ProfileTweet-action--reply u-hiddenVisually">
       <span class="ProfileTweet-actionCount" aria-hidden="true" data-tweet-stat-count="0">
-        <span class="ProfileTweet-actionCountForAria" id="profile-tweet-action-reply-count-aria-1090341261830045696">0 replies</span>
+        <span class="ProfileTweet-actionCountForAria" id="profile-tweet-action-reply-count-aria-${tweet.id_str}">0 replies</span>
       </span>
     </span>
     <span class="ProfileTweet-action--retweet u-hiddenVisually">
       <span class="ProfileTweet-actionCount" aria-hidden="true" data-tweet-stat-count="0">
-        <span class="ProfileTweet-actionCountForAria" id="profile-tweet-action-retweet-count-aria-1090341261830045696">0 retweets</span>
+        <span class="ProfileTweet-actionCountForAria" id="profile-tweet-action-retweet-count-aria-${tweet.id_str}">0 retweets</span>
       </span>
     </span>
     <span class="ProfileTweet-action--favorite u-hiddenVisually">
       <span class="ProfileTweet-actionCount" data-tweet-stat-count="12">
-        <span class="ProfileTweet-actionCountForAria" id="profile-tweet-action-favorite-count-aria-1090341261830045696" data-aria-label-part="">12 likes</span>
+        <span class="ProfileTweet-actionCountForAria" id="profile-tweet-action-favorite-count-aria-${tweet.id_str}" data-aria-label-part="">12 likes</span>
       </span>
     </span>
   </div>
 
   <div class="ProfileTweet-actionList js-actions" role="group" aria-label="Tweet actions">
     <div class="ProfileTweet-action ProfileTweet-action--reply">
-  <button class="ProfileTweet-actionButton js-actionButton js-actionReply" data-modal="ProfileTweet-reply" type="button" aria-describedby="profile-tweet-action-reply-count-aria-1090341261830045696">
+  <button class="ProfileTweet-actionButton js-actionButton js-actionReply" data-modal="ProfileTweet-reply" type="button" aria-describedby="profile-tweet-action-reply-count-aria-${tweet.id_str}">
     <div class="IconContainer js-tooltip" title="Reply">
       <span class="Icon Icon--medium Icon--reply"></span>
       <span class="u-hiddenVisually">Reply</span>
@@ -162,7 +160,7 @@ const imageTweetTemplate = tweet => `<li class="js-stream-item stream-item strea
 </div>
 
     <div class="ProfileTweet-action ProfileTweet-action--retweet js-toggleState js-toggleRt">
-  <button class="ProfileTweet-actionButton  js-actionButton js-actionRetweet" data-modal="ProfileTweet-retweet" type="button" aria-describedby="profile-tweet-action-retweet-count-aria-1090341261830045696">
+  <button class="ProfileTweet-actionButton  js-actionButton js-actionRetweet" data-modal="ProfileTweet-retweet" type="button" aria-describedby="profile-tweet-action-retweet-count-aria-${tweet.id_str}">
     <div class="IconContainer js-tooltip" title="Retweet">
       <span class="Icon Icon--medium Icon--retweet"></span>
       <span class="u-hiddenVisually">Retweet</span>
@@ -185,7 +183,7 @@ const imageTweetTemplate = tweet => `<li class="js-stream-item stream-item strea
 
 
     <div class="ProfileTweet-action ProfileTweet-action--favorite js-toggleState">
-  <button class="ProfileTweet-actionButton js-actionButton js-actionFavorite" type="button" aria-describedby="profile-tweet-action-favorite-count-aria-1090341261830045696">
+  <button class="ProfileTweet-actionButton js-actionButton js-actionFavorite" type="button" aria-describedby="profile-tweet-action-favorite-count-aria-${tweet.id_str}">
     <div class="IconContainer js-tooltip" title="Like">
       <span role="presentation" class="Icon Icon--heart Icon--medium"></span>
       <div class="HeartAnimation"></div>
